e64cb0aded0624285f851e0ad26e8d4a8f7116ac
Vikas Meel <meel.vikas02@gmail.com>
2024-05-04 15:16:56 +0530

    made a qr code generator

---
+++ script.js
@@ -0,0 +1,18 @@
+const QrInput = document.getElementById('qrInput');
+const QrImage = document.getElementById('qrImg')
+const QrButton = document.getElementById('qrButton');
+
+console.log(QrInput, QrImage, QrButton);
+
+qrButton.addEventListener('click' , ()=>{
+
+    const inputValue = QrInput.value;
+    console.log(inputValue);
+
+    if (!inputValue) {
+        alert("Please enter a Valid URL!")
+    } else{
+        QrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
+        QrImage.alt = `QR image for ${inputValue}`
+    }
+})
